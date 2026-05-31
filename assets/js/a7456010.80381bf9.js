97c7af05d2c52859b2b9c228ad0ce4fff0ddb5ac
dmytro-stasyuk <dmytro-stasyuk@users.noreply.github.com>
2026-05-31 16:29:06 +0000

    deploy: dmytro-stasyuk/spec-bright-website@65547bf5173671e6eb7076b5b08e0ddfef737e1e

---
+++ assets/js/a7456010.80381bf9.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkspec_bright_website=globalThis.webpackChunkspec_bright_website||[]).push([[235],{8552(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
